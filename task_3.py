41066a6785104a790837134d411fa42047745075
kos48 <nikitin.kos.48@yandex.ru>
2021-01-20 12:49:22 +0300

    add task 3 4

--- task_3.py
+++ task_3.py
@@ -1,3 +1,16 @@
 '''Реализовать функцию my_func(), которая принимает три позиционных аргумента,
 и возвращает сумму наибольших двух аргументов.'''
 
+def sum_max(a, b, c):
+    z = min(a, b, c)
+    if z == a:
+        result = b + c
+    elif z == b:
+        result = a + c
+    else:
+        a + b
+    return result
+
+print(sum_max(8,3,3))
+
+
